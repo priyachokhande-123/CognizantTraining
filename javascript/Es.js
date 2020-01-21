a6489694b39769b0b5e18f81fa07b703976d87a6
Priya Chokhande <60029368+priyachokhande-123@users.noreply.github.com>
2020-01-21 01:20:14 -0800

    Add files via upload

---
+++ javascript/Es.js
@@ -0,0 +1,51 @@
+console.log("hello");
+ var a="Hello";
+console.log(a); 
+console.log(b);
+var b="hello World";
+a=23.5;
+console.log(a);
+var c;
+console.log(c);
+var str="Hello";
+var age=22;
+var gpa= 3.5;
+var dummy =null;
+console.log(typeof(str));
+console.log(typeof(gpa));
+console.log(typeof(dummy));
+console.log(typeof(xyz));
+console.log(typeof "some data");
+
+var a=20;
+var b=30;
+
+if(a>b)
+{
+    console.log("a is greater");
+}
+else{
+    console.log("b is greater");
+}
+var job="developer";
+
+switch(job){
+    case 'designer':console.log("Needs Creativity...");break;
+    case 'developer':console.log("Must be logically sound!!!");break;
+    default : console.log("Must be adaptable...");
+}
+switch(a){
+    case b> 20: console.log("test"); break;
+    case b > 20 && b<30: console.log("test"); break;
+    case b != 100: console.log("test"); break;
+    case 30: case a > 20: console.log("test"); break;
+}
+var i = 1;
+while(i<=5){
+    console.log("Value : " + i);
+    i++;
+}
+for(j=0;j<=5;j++)
+{
+console.log("value :" +j);
+}
---
+++ javascript/Type.js
@@ -0,0 +1,9 @@
+console.log("hello");
+ var a="Hello";
+console.log(a); 
+console.log(b);
+var b="hello World";
+a=23.5;
+console.log(a);
+var c;
+console.log(c);

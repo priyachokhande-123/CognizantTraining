3be45ae65e9c75eb13449efb9832063689ba171f
Priya Chokhande <60029368+priyachokhande-123@users.noreply.github.com>
2020-01-28 04:17:19 -0800

    Add files via upload

---
+++ Search/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+
+
+const routes: Routes = [
+
+{ path:"", redirectTo : "home", pathMatch: "full"},
+{ path:"home", component: HomeComponent},
+{ path:"contactus", component:ContactComponent},
+{ path:"aboutus", component:AboutComponent},
+{ path:"search/:searchText", component: SearchComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],//registering route
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
